4a6017f166bf77d75c3d38172a8e796c09aaed83
zinnia tasnim <zinnia660@gmail.com>
2025-11-08 00:58:06 +0600

    Import pandas and load CSV data
    Added functionality to read data from a CSV file.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title('C-section Prediction App')
 
 st.info('This is an app to run machine learning model.')
+
+df = pd.read_csv("https://raw.githubusercontent.com/zinniatasnim/data/refs/heads/main/cleaned.csv")
+df
